8668d127ea1d6c5cdb24d7a71f42858e08359054
DongJin-Lee777 <leik24@naver.com>
2020-08-14 22:56:55 +0900

    a

---
+++ food.js
@@ -0,0 +1,41 @@
+$(function() {
+    var topArrow = $("#topArrow");
+
+    $("ul#menu_imgs li img").click(function() {
+        var target = $(this).attr("src").substring(8).split(".")[0];
+        $(".action").removeClass('action');
+        $("#"+target).addClass('action');
+    })
+
+    $(window).scroll(function() {
+        var current_scroll = $(window).scrollTop();
+        if(current_scroll > 755) {
+            topArrow.show();
+        } else {
+            topArrow.hide();
+        }
+    })
+
+    $("#topArrow").click(function() {
+        $( 'html, body' ).animate( { scrollTop : 0 }, 500 );
+        return false;
+    })
+
+    $(window).resize(function(){
+        var w = $(window).width();
+        var w_half = w/2;
+
+        var img_w = $("#brand_img").width();
+        var map_w = $("#map").width();
+
+        var margin_value = [w_half-img_w, w_half-map_w];
+
+        if(w > 1103){
+            $("#brand_img").css("margin-left", margin_value[0]+"px");
+            $("#map").css("margin-left", margin_value[1]+"px");
+        } else {
+            $("#brand_img").css("margin-left", 0+"px");
+            $("#map").css("margin-left", 0+"px");
+        }
+    })
+})
